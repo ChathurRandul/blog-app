901f33b71d90cb23f70f5dc60e2c93e1da827792
Chathur <chathur.randul@gmail.com>
2020-03-11 11:22:52 +0530

    Initial Commit

---
+++ src/build/precache-manifest.ce516a5bc871e682e67df38ad525dce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1950f66ceb4256e4cef6381dea2c14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3090a4a2fed650f1eff",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "08d0cc84f3aa4ce96594",
+    "url": "/static/js/2.e8d3c78d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e8d3c78d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3090a4a2fed650f1eff",
+    "url": "/static/js/main.15f830c6.chunk.js"
+  },
+  {
+    "revision": "11d4487967374bf3f93e",
+    "url": "/static/js/runtime-main.4c888964.js"
+  }
+]);
